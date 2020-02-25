ac9f4b2872fae27a52b0adcb56ae64d3db652c69
Eric Settels and Keturah Howard <eric+keturah@>
2020-02-25 11:22:47 -0800

    modify code to reverse the string without built in method

--- Palindromes.cs
+++ Palindromes.cs
@@ -9,8 +9,16 @@ public class Palindromes
     Console.WriteLine("Enter a word and find out if its a palindrome:");
     string palInput = Console.ReadLine();
     char[] palArr = palInput.ToCharArray();    
-    Array.Reverse(palArr);
-    string reversed = new string(palArr);
+    char[] reversedArr = new char[palArr.GetLength(0)];
+    int j = 0;
+    for(int i = reversedArr.GetLength(0)-1; i >= 0; i-- )
+    {
+      reversedArr[j] = palArr[i];
+      Console.WriteLine(reversedArr[j]);
+      j++;
+    }
+    // Array.Reverse(palArr);
+     string reversed = new string(reversedArr);
     if(palInput == reversed)
     {
       Console.WriteLine($"your word: '{palInput}' is '{reversed}' when is backwards so it works! ");
